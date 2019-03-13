a0ed6c3069c2c1f9d4867e03090d7d9e4ca8660d
loganballard <33067676+loganballard@users.noreply.github.com>
2019-03-13 13:06:39 -0700

    fixing driver

--- driver.sh
+++ driver.sh
@@ -2,20 +2,21 @@
 
 pip install -r requirements.txt
 
+[ -z "$PGDB" ]              && export PGDB="postgres"
+[ -z "$PGUSERNAME" ]        && export PGUSERNAME="postgres"
+[ -z "$PGPASSWORD" ]        && export PGPASSWORD="admin"
+[ -z "$PGHOST" ]            && export PGHOST="localhost"
+[ -z "$PGPORT" ]            && export PGPORT="5432"
+[ -z "$NFLDBSTARTYEAR" ]    && export NFLDBSTARTYEAR=2010
+[ -z "$NFLDBENDYEAR" ]      && export NFLDBENDYEAR=2019
+[ -z "$NFLSUBDB" ]          && export NFLSUBDB="nflDb"
+
 echo 'Make a choice:'
 echo '    1. Intial DB setup (WARNING THIS TAKES A LONG TIME ONLY RUN ONCE!)'
 echo '    2. Run front end'
 read USERCHOICE
 
 if [ "$USERCHOICE" -eq 1 ]; then
-    [ -z "$PGDB" ]              && export PGDB="postgres"
-    [ -z "$PGUSERNAME" ]        && export PGUSERNAME="postgres"
-    [ -z "$PGPASSWORD" ]        && export PGPASSWORD="admin"
-    [ -z "$PGHOST" ]            && export PGHOST="localhost"
-    [ -z "$PGPORT" ]            && export PGPORT="5432"
-    [ -z "$NFLDBSTARTYEAR" ]    && export NFLDBSTARTYEAR=2010
-    [ -z "$NFLDBENDYEAR" ]      && export NFLDBENDYEAR=2019
-    [ -z "$NFLSUBDB" ]          && export NFLSUBDB="nflDb"
     cd setup
     python db_init.py
     cd ..
