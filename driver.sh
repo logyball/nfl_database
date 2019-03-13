475aa51840d6b631d89aeeedac645f3e3971500c
loganballard <33067676+loganballard@users.noreply.github.com>
2019-03-13 13:07:39 -0700

    fixing driver

--- driver.sh
+++ driver.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-pip install -r requirements.txt
-
 [ -z "$PGDB" ]              && export PGDB="postgres"
 [ -z "$PGUSERNAME" ]        && export PGUSERNAME="postgres"
 [ -z "$PGPASSWORD" ]        && export PGPASSWORD="admin"
@@ -17,6 +15,7 @@ echo '    2. Run front end'
 read USERCHOICE
 
 if [ "$USERCHOICE" -eq 1 ]; then
+    pip install -r requirements.txt
     cd setup
     python db_init.py
     cd ..
